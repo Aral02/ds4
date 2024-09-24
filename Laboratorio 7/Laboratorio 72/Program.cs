4715aeb13f5a8985e759f3fe1364608ab3e17225
Aral02 <Arielchong.198@gmail.com>
2024-09-24 16:42:20 -0500

    Laboratorio 7 Completo

---
+++ Laboratorio 7/Laboratorio 72/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Laboratorio_72;
+
+class Dado
+{
+    private int Valor;
+    private static Random aleatorio;
+
+    public Dado()
+    {
+        aleatorio = new Random();
+    }
+    public void Tirar()
+    {
+        Valor = aleatorio.Next(1,7);
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("El valor del dado es: " + Valor);
+    }
+    public int RetornarValor()
+    {
+        return Valor;
+    }
+}
+
+class JuegoDeDados
+{
+    private Dado dado1, dado2, dado3;
+
+    public JuegoDeDados()
+    {
+        dado1 = new Dado();
+        dado2 = new Dado();
+        dado3 = new Dado();
+    }
+
+    public void jugar()
+    {
+        dado1.Tirar();
+        dado1.Imprimir();
+        dado2.Tirar();
+        dado2.Imprimir();
+        dado3.Tirar();
+        dado3.Imprimir();
+        if (dado1.RetornarValor() == dado2.RetornarValor() && dado1.RetornarValor == dado3.RetornarValor)
+        {
+            Console.WriteLine("GANÓ");
+        }
+        else
+        {
+            Console.WriteLine("PERDIÓ");
+        }
+        Console.ReadKey();
+    }
+}
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        JuegoDeDados j = new JuegoDeDados();
+        j.jugar();
+    }
+}
---
+++ Laboratorio 7/Program.cs
@@ -0,0 +1,17 @@
+﻿/*                                      Desarrollo de Software IV
+Laboratorio # 7 – Prof. Regis Rivera
+Objetivo: Introducción a POO en C#
+PARAMETROS
+ IDE: Visual Studio .NET 2022
+ Tipo de Aplicación: Consola
+ Solución: Laboratorio7
+ Proyectos: Laboratorio71, 72… [Según cada Ejemplo]
+ Lenguaje: C#
+                                            LABORATORIOS
+Lab 7 - 1
+Un banco tiene 3 clientes que pueden hacer depósitos y extracciones. También el banco requiere
+que al final del día calcule la cantidad de dinero que hay depositada.
+---------------------------------------------------------------------------------------------------------
+Lab 7-2
+Plantear un programa que permita jugar a los dados. Las reglas de juego son: se tiran tres dados si
+los tres salen con el mismo valor mostrar un mensaje que "gano", sino "perdió".*/
